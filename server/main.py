f2eedbce145b765bd16d84097ca1374cccbe346b
radyvoniuk <radyvoniukkate@gmail.com>
2025-06-17 14:54:53 +0300

    main change

--- server/main.py
+++ server/main.py
@@ -1,22 +1,27 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app import router
 from auth import router as auth_router
 
-
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173",  
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],    
-    allow_headers=["*"],    
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
 app.include_router(auth_router)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
